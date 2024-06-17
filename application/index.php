6d890ec538cb80df514ea6b16df337d94573d9c7
Irene Archimbaud <irene@getinov.com>
2024-06-17 15:00:25 +0200

    add inscription page and modify style

--- application/index.php
+++ application/index.php
@@ -4,7 +4,7 @@
             <meta charset="utf-8">
             <title>Accueil</title>
             <link rel="stylesheet" href="style.css">
-            <script src="script.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/css-doodle/0.39.2/css-doodle.min.js"></script>
         </head>
         <body>
             <?php
--- application/inscription.php
+++ application/inscription.php
@@ -1,7 +1,7 @@
 <html lang="fr">
         <head>
             <meta charset="utf-8">
-            <title>Connexion</title>
+            <title>Inscription</title>
             <link rel="stylesheet" href="./style/style.css">
             <script src="https://cdnjs.cloudflare.com/ajax/libs/css-doodle/0.39.2/css-doodle.min.js"></script>
         </head>
@@ -11,17 +11,23 @@
             ?>
             <h1 class="productstock">ProductStock</h1>
             <div class="connexion-form-page">
-            <h1>Connexion</h1>
-                <form action="connexion_post.php" method="POST">
+            <h1>Inscription</h1>
+                <form action="inscription_post.php" method="POST">
+                    <label for="lastName">Nom:</label>
+                    <input type="text" id="lastName" name="lastName" />
+                    <label for="firstName">Prénom:</label>
+                    <input type="text" id="firstName" name="firstName" />
                     <label for="username">Identifiant:</label>
                     <input type="text" id="username" name="username" />
                     <label for="password">Mot de passe:</label>
                     <input type="password" id="password" name="password" />
+                    <label for="password">Confirmer le mot de passe:</label>
+                    <input type="password" id="password" name="password" />
                     <div class="form-button-div">
-                        <input type="submit" value="Se connecter" />
+                        <input type="submit" value="Inscription" />
                     </div>
                 </form>
-                Vous n'avez pas de compte ? <a class="inscription-button" href="./inscription.php">Inscrivez-vous</a>
+                Vous avez déjà un compte ? <a class="inscription-button" href="./connexion.php">connectez-vous</a>
             </div>
             <?php
                 require_once './footer.php';
